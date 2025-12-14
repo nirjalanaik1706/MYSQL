824ea40929dfa2962971551bbeba9eed3d3eb606
NIRJALA DATTATRAY NAIK <158537275+nirjalanaik1706@users.noreply.github.com>
2025-12-14 21:39:14 +0530

    Update SPDB.sql

--- tflassessment_database/SPDB.sql
+++ tflassessment_database/SPDB.sql
@@ -299,3 +299,26 @@ END $$
 DELIMITER ;
 
 CALL GetTestEmployeeDetailsByCandidate(6);
+
+CREATE PROCEDURE GetTestEmployeeDetailsByCandidate(IN candidate INT)
+BEGIN
+    SELECT 
+        t.id,
+        ts.candidateid,
+        t.name AS testname,
+        t.passinglevel,
+        t.duration,
+        ts.scheduledstart,
+        ts.scheduledend,
+        ts.status
+    FROM 
+        testschedules ts
+    JOIN 
+        tests t ON ts.testid = t.id
+    WHERE 
+        ts.candidateid = candidate;
+END $$
+
+DELIMITER ;
+
+CALL GetTestEmployeeDetailsByCandidate(6);
