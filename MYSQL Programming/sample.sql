2a80cb41523421c62485a4c65b5fa7424df47cad
NIRJALA DATTATRAY NAIK <158537275+nirjalanaik1706@users.noreply.github.com>
2025-12-15 18:48:23 +0530

    Update sample.sql

--- MYSQL Programming/sample.sql
+++ MYSQL Programming/sample.sql
@@ -9,7 +9,12 @@ insert into em(empid,fnm,lnm) values(1,"sahil","kamble");
 insert into em(empid,fnm,lnm) values(2,"nirjala","naik");
 insert into em(empid,fnm,lnm) values(3,"saloni","kamble");
 insert into em(empid,fnm,lnm) values(1,"sahil","kamble ");
-insert into em(empid,fnm,lnm) values(4,"saya","naik");
+insert into em(empid,fnm,lnm) values(4,"saya1","naik");
+insert into em(empid,fnm,lnm) values(1,"sahil1","kamble");
+insert into em(empid,fnm,lnm) values(2,"nirjala1","naik");
+insert into em(empid,fnm,lnm) values(3,"saloni1","kamble");
+insert into em(empid,fnm,lnm) values(1,"sahil1","kamble ");
+insert into em(empid,fnm,lnm) values(4,"saya1","naik");
 select * from em;
 
 
